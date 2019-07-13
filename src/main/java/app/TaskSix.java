52c5cd857c91802b16cfb0429c052289c354b455
lqiulin <1515867465@qq.com>
2019-07-14 01:34:41 +0800

    finish taskSix-2

--- src/main/java/app/TaskSix.java
+++ src/main/java/app/TaskSix.java
@@ -59,21 +59,33 @@ public class TaskSix {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException{
         Configuration conf = new Configuration();
+        conf.set("mapreduce.totalorderpartitioner.naturalorder", "false");
         Job job = Job.getInstance(conf, "task6-1");
         job.setJarByClass(TaskSix.class);
 
         job.setInputFormatClass(KeyValueTextInputFormat.class);
         //设置比较器，用于比较数据的大小，然后按顺序排序，该例子主要用于比较两个key的大小
-
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 
         //设置保存partitions文件的路径
-        Path partitionFile = new Path(new Path(args[0]), "_partitions");
-        TotalOrderPartitioner.setPartitionFile(conf, partitionFile);
+        TotalOrderPartitioner.setPartitionFile(job.getConfiguration(), new Path("partitions.txt"));
+    //    Path partitionFile = new Path(new Path(args[0]), "_partitions");
+    //    TotalOrderPartitioner.setPartitionFile(conf, partitionFile);
 
         //key值采样，0.01是采样率，
-        InputSampler.Sampler<Text, Text> sampler = new InputSampler.RandomSampler<>(0.01, 1000, 100);
+        InputSampler.Sampler<Text, Text> sampler = new InputSampler.RandomSampler<>(0.5, 1000, 100);
+        InputSampler.writePartitionFile(job, sampler);
 
+
+
+        //采样器：三个参数
+        /* 第一个参数 freq: 表示来一个样本，将其作为采样点的概率。如果样本数目很大
+         *第二个参数 numSamples：表示采样点最大数目为，我这里设置10代表我的采样点最大为10，如果超过10，那么每次有新的采样点生成时
+         * ，会删除原有的一个采样点,此参数大数据的时候尽量设置多一些
+         * 第三个参数 maxSplitSampled：表示的是最大的分区数：我这里设置100不会起作用，因为我设置的分区只有4个而已
+*/
 
         job.setMapperClass(TaskSix.TaskSixMapper.class);
         //      job.setCombinerClass(InvertedIndex.InvertedIndexCombiner.class);
@@ -86,8 +98,7 @@ public class TaskSix {
 
 
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
