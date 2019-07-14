63b7a20b6aca71c9831faa60b4df90fd872e11d1
cjinchi <cjinchi@qq.com>
2019-07-14 15:53:00 +0800

    task three test PASSed

--- src/main/java/app/TaskThree.java
+++ src/main/java/app/TaskThree.java
@@ -8,7 +8,6 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
@@ -16,7 +15,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 
 public class TaskThree {
@@ -25,93 +23,106 @@ public class TaskThree {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            Text name = new Text();
-            IntWritable number = new IntWritable();
-            if (itr.hasMoreTokens())
-                name.set(itr.nextToken());
-            if (itr.hasMoreTokens())
-                number.set(Integer.parseInt(itr.nextToken()));
-            context.write(name, number);
+//            StringTokenizer itr = new StringTokenizer(value.toString());
+//            Text name = new Text();
+//            IntWritable number = new IntWritable();
+//            if (itr.hasMoreTokens())
+//                name.set(itr.nextToken());
+//            if (itr.hasMoreTokens())
+//                number.set(Integer.parseInt(itr.nextToken()));
+            String[] items = value.toString().trim().split("\t");
+            if (items.length != 2) {
+                throw new RuntimeException();
+            }
+            context.write(new Text(items[0]), new IntWritable(Integer.valueOf(items[1])));
         }
 
     }
 
     public static class TaskThreePartitioner extends HashPartitioner<Text, IntWritable> {
-        private Text term = new Text();
 
         @Override
         public int getPartition(Text key, IntWritable value, int numReduceTasks) {
-            term.set(key.toString().split(",")[0]);
-            return super.getPartition(term, value, numReduceTasks);
+//            term.set(key.toString().split(",")[0]);
+            String[] items = key.toString().trim().split(",");
+            if (items.length != 2 || !items[0].startsWith("<")) {
+                throw new RuntimeException();
+            }
+            return super.getPartition(new Text(items[0].substring(1)), value, numReduceTasks);
         }
     }
 
     public static class TaskThreeReducer extends Reducer<Text, IntWritable, Text, Text> {
         //类似于temp变量，用于把String转为Text
-        private Text str1 = new Text();
-        private Text str2 = new Text();
+//        private Text str1 = new Text();
+//        private Text str2 = new Text();
 
-        /**
-         * 由于一个key [即term+filename]调用一次reduce()函数，因此同一个term实际上需要调用多次reduce函数才能处理完毕。
-         * currentTerm表示当前正在处理的term，当reduce函数中判断到term和currentTerm不相同时，说明已经开始处理下一个term了。
-         * currentPostings表示currentTerm的索引，当currentTerm变化时需要清空。
-         */
-        private String previousName = null;
+        private String currentName = null;
         private int currentCount = 0;
 
 
-        private Map<String, Integer> postings = new LinkedHashMap<>();
+        private Map<String, Integer> postings = new HashMap<>();
 
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            String twoNames = key.toString();
-            String[] names = twoNames.substring(1, twoNames.length() - 1).split(",");
-            String firstName = names[0];
-            String secondName = names[1];
-            if (previousName == null || !previousName.equals(firstName)) {
-                writeCurrentName(context);
-                previousName = firstName;
-                postings.clear();
-                currentCount = 0;
+            String keyStr = key.toString();
+            if(!keyStr.startsWith("<")||!keyStr.endsWith(">") ){
+                throw new RuntimeException();
             }
+            String[] items = keyStr.substring(1, keyStr.length()-1).split(",");
+            if(items.length!=2){
+                throw new RuntimeException();
+            }
+
+            if(currentName == null){
+                currentName = items[0];
+                currentCount = 0;
+                postings.clear();
+            }else if(!currentName.equals(items[0])){
+                writeCurrentName(context);
+
+                currentName = items[0];
+                currentCount = 0;
+                postings.clear();
+            }
+
             int sum = 0;
             for (IntWritable value : values) {
                 sum += value.get();
             }
-            currentCount = currentCount + sum;
-            postings.put(secondName, sum);
+            currentCount+=sum;
+            postings.put(items[1], sum);
 
         }
 
         @Override
         public void cleanup(Context context) throws IOException, InterruptedException {
-                writeCurrentName(context);
+            writeCurrentName(context);
         }
 
         private void writeCurrentName(Context context) throws IOException, InterruptedException {
-            if(previousName == null){
+            if (currentName == null) {
                 return;
             }
-            double percent = 1.0 / currentCount;
-            StringBuilder postingsBuilder = new StringBuilder();
-            postingsBuilder.append('[');
-            int index = 0;
+            double total = currentCount;
+//            double percent = 1.0 / currentCount;
+            StringBuilder builder = new StringBuilder();
+            builder.append('[');
+//            int index = 0;
             for (Map.Entry<String, Integer> entry : postings.entrySet()) {
-                //     System.out.println("key="+entry.getKey()+",value = "+entry.getValue());
-                if (index == 0)
-                    index = 1;
-                else
-                    postingsBuilder.append('|');
-                postingsBuilder.append(entry.getKey());
-                postingsBuilder.append(',');
-                postingsBuilder.append(entry.getValue() * percent);
+                builder.append(entry.getKey());
+                builder.append(',');
+                builder.append(entry.getValue() / total);
+                builder.append('|');
             }
-            postingsBuilder.append(']');
-            str1.set(previousName);
-            str2.set(postingsBuilder.toString());
-            System.out.println(previousName+postingsBuilder.toString());
-            context.write(str1, str2);
+            if(builder.length()>1){
+                builder.deleteCharAt(builder.length()-1);
+            }
+            builder.append(']');
+//            str1.set(currentName);
+//            str2.set(builder.toString());
+//            System.out.println(currentName + builder.toString());
+            context.write(new Text(currentName),new Text(builder.toString()));
         }
 
     }
